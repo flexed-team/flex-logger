994698f5be3512c4465622d026a6289a94420178
hadrizi <vbyhgn@.com>
2020-02-15 18:18:22 +0300

    Removed .cpp file, moved all definitions to header

--- src/include/flex-logger.h
+++ src/include/flex-logger.h
@@ -1,8 +1,12 @@
 #ifndef FLEX_LOGGER_H_
 #define FLEX_LOGGER_H_
+#pragma warning(disable : 4996)
 
-#include<windows.h>
-#include<string>
+#include <windows.h>
+#include <string>
+#include <windows.h>
+#include <iostream>
+#include <time.h>
 
 #define C_RED     FOREGROUND_RED
 #define C_GREEN   FOREGROUND_GREEN
@@ -20,9 +24,62 @@ namespace flexLogger
 
 		~Logger() {};
 
-		void log(const char*);
-		void log_err(const char*);
-		void log_warr(const char*);
+		void log(const char* msg)
+		{
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(hConsole, C_GREEN);
+
+			time_t raw_now;
+			struct tm* timedata;
+			char   buffer[255];
+
+			time(&raw_now);
+			timedata = localtime(&raw_now);
+
+			strftime(buffer, sizeof(buffer), "%H:%M:%S", timedata);
+
+			std::cout << "[" << buffer << "] " << name << " : " << msg << std::endl;
+
+			SetConsoleTextAttribute(hConsole, C_DEFAULT);
+		};
+
+		void log_err(const char* msg)
+		{
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(hConsole, C_RED);
+
+			time_t raw_now;
+			struct tm* timedata;
+			char   buffer[255];
+
+			time(&raw_now);
+			timedata = localtime(&raw_now);
+
+			strftime(buffer, sizeof(buffer), "%H:%M:%S", timedata);
+
+			std::cout << "[" << buffer << "] " << name << " : " << msg << std::endl;
+
+			SetConsoleTextAttribute(hConsole, C_DEFAULT);
+		};
+
+		void log_warr(const char* msg)
+		{
+			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(hConsole, C_YELLOW);
+
+			time_t raw_now;
+			struct tm* timedata;
+			char   buffer[255];
+
+			time(&raw_now);
+			timedata = localtime(&raw_now);
+
+			strftime(buffer, sizeof(buffer), "%H:%M:%S", timedata);
+
+			std::cout << "[" << buffer << "] " << name << " : " << msg << std::endl;
+
+			SetConsoleTextAttribute(hConsole, C_DEFAULT);
+		};
 
 	protected:
 		const char* name;
